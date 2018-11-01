db97c376d107a6796630ed96ac353b6a0d499614
Itai Chejanovsky <itaich@wix.com>
2018-11-01 14:11:21 +0200

    Fixed links so that the demo can work in github pages

--- src/js/configuration.js
+++ src/js/configuration.js
@@ -1,76 +1,98 @@
 (function () {
-    window.childFrames = [];
-    window.couriers = [];
-    window.channels = new Chronos.Channels({ externalProxy: true });
+  window.childFrames = [];
+  window.couriers = [];
+  window.channels = new Chronos.Channels({externalProxy: true});
+  window.state = {
+    insane: false
+  };
 
-    bindEvent({
-        element: window, eventName: "load", func: function () {
+  bindEvent({
+    element: window, eventName: "load", func: function () {
 
-            _buildDOM();
+      _buildDOM();
 
-            bindEvent({
-                element: "#btnAnimateIt",
-                func: function () {
-                    var elementsSelection = ["iframe", "img"].concat(templates.keys).concat(randomizer.rotationClasses.slice(1));
-                    var animationType = getRandomFromArray(animationAPI.classes);
-                    var elementsType = getRandomFromArray(elementsSelection);
-
-                    if (elementsType !== "iframe" && elementsType !== "img") {
-                        elementsType = "." + elementsType;
-                    }
-                    animationAPI.animate({selector: elementsType, animation: {className: animationType}});
-                }
-            });
+      bindEvent({
+        element: "#btnAnimateIt",
+        func: function () {
+          animate();
         }
-    });
+      });
 
-    function _buildDOM() {
-        if (window.templates.keys) {
-            randomizer.generate();
-
-            childFrames.push(addIFrame("http://localhost/pluggableUI/src/children/cow.html?lpHost=" + encodeURIComponent("http://" + location.hostname), {
-                position: "fixed",
-                top: "10px",
-                right: "5px",
-                height: "81px"
-            }));
-
-            childFrames.push(addIFrame("http://localhost/pluggableUI/src/children/sheep.html?lpHost=" + encodeURIComponent("http://" + location.hostname), {
-                position: "fixed",
-                bottom: "10px",
-                right: "5px",
-                height: "117px"
-            }));
-
-            childFrames.push(addIFrame("http://localhost/pluggableUI/src/children/crab.html?lpHost=" + encodeURIComponent("http://" + location.hostname), {
-                position: "fixed",
-                bottom: "10px",
-                left: "5px",
-                height: "95px"
-            }));
-
-            while (childFrames.length > 0) {
-                couriers.push(new Chronos.PostMessageCourier({
-                    eventChannel: channels,
-                    target: childFrames.shift(),
-                    targetOrigin: location.protocol + "//localhost"
-                }));
-            }
-        } else {
-            setTimeout(_buildDOM, 200);
+      bindEvent({
+        element: "#insane",
+        func: () => {
+          window.state.insane = !window.state.insane;
+          animateConstantly();
         }
+      });
     }
+  });
 
-    function addIFrame(URL, style) {
-        var ifr = document.createElement("IFRAME");
-        ifr.src = URL;
-        for (var key in style) {
-            if (style.hasOwnProperty(key)) {
-                ifr.style[key] = style[key];
-            }
-        }
-        document.body.appendChild(ifr);
-        return ifr;
+  function animate(){
+    const elementsSelection = ["iframe", "img"].concat(templates.keys).concat(randomizer.rotationClasses.slice(1));
+    const animationType = getRandomFromArray(animationAPI.classes);
+    let elementsType = getRandomFromArray(elementsSelection);
+
+    if (elementsType !== "iframe" && elementsType !== "img") {
+      elementsType = "." + elementsType;
     }
+    animationAPI.animate({selector: elementsType, animation: {className: animationType}});
+  }
+
+  function animateConstantly(){
+    if(window.state.insane){
+        animate();
+        setTimeout(animateConstantly, 200);
+    }
+  }
+
+  function _buildDOM() {
+    if (window.templates.keys) {
+      randomizer.generate();
+
+      childFrames.push(addIFrame("children/cow.html?lpHost=" + encodeURIComponent("http://" + location.hostname), {
+        position: "fixed",
+        top: "10px",
+        right: "5px",
+        height: "81px"
+      }));
+
+      childFrames.push(addIFrame("children/sheep.html?lpHost=" + encodeURIComponent("http://" + location.hostname), {
+        position: "fixed",
+        bottom: "10px",
+        right: "5px",
+        height: "117px"
+      }));
+
+      childFrames.push(addIFrame("children/crab.html?lpHost=" + encodeURIComponent("http://" + location.hostname), {
+        position: "fixed",
+        bottom: "10px",
+        left: "5px",
+        height: "95px"
+      }));
+
+      while (childFrames.length > 0) {
+        couriers.push(new Chronos.PostMessageCourier({
+          eventChannel: channels,
+          target: childFrames.shift(),
+          targetOrigin: location.protocol + "//localhost"
+        }));
+      }
+    } else {
+      setTimeout(_buildDOM, 200);
+    }
+  }
+
+  function addIFrame(URL, style) {
+    const ifr = document.createElement("IFRAME");
+    ifr.src = URL;
+    for (let key in style) {
+      if (style.hasOwnProperty(key)) {
+        ifr.style[key] = style[key];
+      }
+    }
+    document.body.appendChild(ifr);
+    return ifr;
+  }
 
 })();
